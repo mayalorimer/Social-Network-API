af615e38c7b8c9633fb1a83ba6746a0af87f911b
Maya Lorimer <maya.lorimer@yahoo.com>
2022-07-26 10:25:43 -0700

    more thought controllers added

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -15,7 +15,7 @@ const thoughtsController = {
     },
 
     // get thought by ID
-    getThoughtById(req, res) {
+    getSingleThought(req, res) {
         Thoughts.findOne({ _id: req.params.id })
         .populate({ path: 'reactions', select: '-__v' })
         .select('-__v')
@@ -33,6 +33,57 @@ const thoughtsController = {
     },
 
 
+    //create a thought
+    createThought(req, res){
+        Thoughts.create(req.body)
+        .then(({_id}) => {
+            return Users.findOneAndUpdate({ _id: req.params.userId},
+                {$push: {thoughts: _id}}, 
+                {new: true});
+        })
+        .then(dbThoughtsData => {
+            if(!dbThoughtsData){
+                res.status(404).json({message: 'Error'});
+                return;
+            }
+            res.json(dbThoughtsData)
+        })
+        .catch(err => res.json(err));
+    },
+
+    // update a thought by ID
+    updateThought(req, res){
+        Thoughts.findOneAndUpdate({_id: req.params.id}, req.body, {new: true, runValidators: true})
+        .populate({path: 'reactions', select: '-__v'})
+        .select('-___v')
+        .then(dbThoughtsData => {
+            if (!dbThoughtsData) {
+                res.status(404).json({message: 'No thoughts with this ID!'});
+                return;
+            }
+                res.json(dbThoughtsData);
+        })
+        .catch(err => res.json(err));
+    },
+
+    // delete a thought
+    deleteThought(req, res){
+        Thoughts.findOneAndDelete({_id: req.params.id})
+        .then(dbThoughtsData => {
+            if (!dbThoughtsData) {
+                res.status(404).json({message: 'No thoughts with this ID!'});
+                return;
+            }
+            res.json(dbThoughtsData);
+            })
+            .catch(err => res.status(400).json(err));
+    },
+
+
+    // add a reaction
+
+
+    // delete a reaction
 
 
 };
